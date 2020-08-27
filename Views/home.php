b033f40e8d57809addc46050dcc03d370b611cac
Osmin Medrano Varela <osmedranov10@yahoo.es>
2020-08-26 21:01:33 -0600

    Done home, styles, index

---
+++ Views/home.php
@@ -0,0 +1,37 @@
+<head>
+  <title>Home</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <div class="container" id="partBody">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-md-9">
+	<h3>¡Experiencias del Deporte!</h3>
+	<p style="color: #000000">
+	  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+	  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+	  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+	  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+	  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+	  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+	  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+	  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+	  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+	  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+	  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+	<h3 style="color: black">¡Cuenta tus experiencias en los blogs!</h3>
+      </div>
+    </div>
+  </div>
+</body>
--- Views/login.php
+++ Views/login.php
@@ -1,36 +1,33 @@
-<html>
-  <head>
-    <title>LOGIN</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-  <body background="Images/cyclin.jpg">
-    <div class="container">
-        <div class="row justify-content-center pt-5 mt-5 m-1">
-            <div class="col-md-6 col-sm-8 col-xl-4 col-lg-5 form1">
-                <form action="">
-                <div class="form-group text-center pt-3">
-                    <h1 class="text-light">INICIAR SESIÓN</h1>
-                </div>
-                    <div class="form-group mx-sm-4 pt-3">
-                        <input type="text" class="form-control" placeholder="Ingrese su correo">
-                    </div>
-                    <div class="form-group mx-sm-4 pb-3">
-                        <input type="text" class="form-control" placeholder="Ingrese su contraseña">
-                    </div>
-                    <div class="form-group mx-sm-4 pb-2">
-                        <input type="submit" class="btn btn-block enter" value="INGRESAR">
-                    </div>
-                    <div class="form-group text-right">
-                        <span><a href="#" class="forgot">Olvidé mi contraseña</a></span>
-                    </div>
-                    <div class="form-group text-center">
-                        <span><a href="" class="forgot1">REGISTRARSE</a></span>
-                    </div>
-                </form>
-            </div>
-        </div>
+<head>
+  <title>LOGIN</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body background="Images/cyclin.jpg">
+  <div class="container">
+    <div class="row justify-content-center pt-5 mt-5 m-1">
+      <div class="col-md-6 col-sm-8 col-xl-4 col-lg-5 form1">
+	<form action="">
+	  <div class="form-group text-center pt-3">
+	    <h1 class="text-light">INICIAR SESIÓN</h1>
+	  </div>
+	  <div class="form-group mx-sm-4 pt-3">
+	    <input type="text" class="form-control" placeholder="Ingrese su correo">
+	  </div>
+	  <div class="form-group mx-sm-4 pb-3">
+	    <input type="text" class="form-control" placeholder="Ingrese su contraseña">
+	  </div>
+	  <div class="form-group mx-sm-4 pb-2">
+	    <input type="submit" class="btn btn-block enter" value="INGRESAR">
+	  </div>
+	  <div class="form-group text-right">
+	    <span><a href="#" class="forgot">Olvidé mi contraseña</a></span>
+	  </div>
+	  <div class="form-group text-center">
+	    <span><a href="" class="forgot1">REGISTRARSE</a></span>
+	  </div>
+	</form>
+      </div>
     </div>
-  </body>
-  
-</html>
+  </div>
+</body>
