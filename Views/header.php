5b1e2f3694c302eb7d00f4f1018eba5d8872f611
Osmin Medrano Varela <osmedranov10@yahoo.es>
2020-08-26 20:27:26 -0600

    Edit files of Views html to php. Add header and footer.

---
+++ Views/header.php
@@ -0,0 +1,29 @@
+<!-- Header-->
+<header class="container-fluid" id="header">
+    <nav class="navbar navbar-expand-lg container">
+        <a class="navbar-brand" href="index.php">
+            <img src="" alt="Logo Inventario">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	  <span class="navbar-toggler-icon"><img src="Images/menuHamburger.png"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php" >Principal<span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="blogsAll">
+                <a class="nav-link" href="?c=productosView" >Productos</a>
+            </li>
+
+            <li class="blogsNews">
+                <a class="nav-link" href="?c=nuevoProducto" >Ingresar Producto</a>
+            </li>
+
+          </ul>                 
+        </div>
+    </nav>  
+</header>
