b0c752508cd611e54619db7a13391d526ec0b370
Osmin Medrano Varela <osmedranov10@yahoo.es>
2020-08-27 21:41:04 -0600

    Configuration in Style with header and fixing other properties

--- Views/header.php
+++ Views/header.php
@@ -1,33 +1,35 @@
 <!-- Header-->
 <header class="container-fluid" id="header">
-    <nav class="navbar navbar-expand-lg container">
-        <a class="navbar-brand" href="index.php">
-            <img src="Images/logo.png" style="width: 105px;">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	  <span class="navbar-toggler-icon"><img src="Images/menuHamburger.png"></span>
-        </button>
+  <nav class="navbar navbar-expand-lg container">
+    <a class="navbar-brand" href="index.php">
+      <img src="Images/logo.png" style="width: 92px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"><img src="Images/menuHamburger.png"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
 
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php" >Principal<span class="sr-only">(current)</span></a>
-            </li>
+	<li class="nav-item active">
+	  <a class="nav-link" href="index.php" >Principal<span class="sr-only">(current)</span></a>
+	</li>
 
-            <li class="blogsAll">
-                <a class="nav-link" href="?c=productosView" >Productos</a>
-            </li>
-
-            <li class="blogsNews">
-                <a class="nav-link" href="?c=nuevoProducto" >Ingresar Producto</a>
-            </li>
-	    
-	    <li class="blogsNews">
-                <a class="nav-link" href="?c=login">Login</a>
-            </li>
-
-          </ul>                 
-        </div>
-    </nav>  
+	<li class="nav-item dropdown">
+	  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	    Tienda
+	  </a>
+	  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	    <a class="dropdown-item" href="#">Ver productos</a>
+	    <a class="dropdown-item" href="#">Carrito</a>
+	    <div class="dropdown-divider"></div>
+	    <a class="dropdown-item" href="#">Something else here</a>
+	  </div>
+	</li>
+      </ul>  
+      <li class="nav-item form-inline my-2 my-lg-0" id="login_position" >
+	  <a class="nav-link" href="?c=login">Login</a>
+	</li>
+    </div>
+  </nav>  
 </header>
