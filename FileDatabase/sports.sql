ea297406c88dca6ef6c96ca58814e00d2dc4a00f
Osmin Medrano Varela <osmedranov10@yahoo.es>
2020-08-30 11:33:34 -0600

    New file of database

--- FileDatabase/sports.sql
+++ FileDatabase/sports.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-08-2020 a las 23:29:16
+-- Tiempo de generación: 29-08-2020 a las 03:27:48
 -- Versión del servidor: 10.4.13-MariaDB
 -- Versión de PHP: 7.4.7
 
@@ -20,6 +20,51 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `sports`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
+--
+
+CREATE TABLE `categories` (
+  `id_categorie` int(11) NOT NULL,
+  `name_categorie` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `categories`
+--
+
+INSERT INTO `categories` (`id_categorie`, `name_categorie`) VALUES
+(1, 'Refrescos'),
+(2, 'Picaritas'),
+(3, 'Abarrotes'),
+(4, 'Linea Blanca');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `id_product` int(11) NOT NULL,
+  `name_product` varchar(255) NOT NULL,
+  `id_categorie` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id_product`, `name_product`, `id_categorie`, `price`, `quantity`, `description`) VALUES
+(1, 'Coca-Cola 2.5 mL', 1, 1200, 32, 'Bebida gaseosa y refrescante vendida a nivel mundial, en tiendas, restaurantes y máquinas expendedoras en más de doscientos países o territorios.'),
+(2, 'Ranchitas Queso', 2, 100, 48, 'Deliciosas tortillas de maíz y queso, horneadas y fritas para darles ese crujiente que tanto te gusta.'),
+(3, 'Refrigeradora doble puerta', 4, 348000, 7, 'Dispositivos electrónicos empleados principalmente en cocinas y en laboratorios, que consisten en un armario aislado térmicamente, con un compartimento principal en el que se mantiene una temperatura de entre 2 y 6 °C.');
 
 -- --------------------------------------------------------
 
@@ -42,13 +87,24 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id_user`, `name`, `last_name`, `email`, `password`, `admin`) VALUES
 (1, '', '', 'admin', 'admin', 1),
-(3, 'Nico', 'Morales', '', '123', 0),
-(4, 'Nico', 'Morales', 'nicomorales1597@gmail.com', '123', 0),
-(5, 'Nico', 'Morales', 'nicomorales1597@gmail.com', '123', 0);
-
+(2, 'Nico', 'Morales', 'nicomorales1597@gmail.com', '123', 0),
+(3, 'Juan', 'Castillo', 'Juan_Castillo@gmail.com', '456', 0);
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id_categorie`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id_product`),
+  ADD KEY `id_categorie` (`id_categorie`);
 
 --
 -- Indices de la tabla `users`
@@ -61,12 +117,45 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id_categorie`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+CREATE TABLE salescar (
+	id_sale INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_user INT NOT NULL,
+    id_product INT(11) NOT NULL,
+    id_categorie INT(11) NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES users(id_user),
+    FOREIGN KEY (id_product) REFERENCES products(id_product),
+    FOREIGN KEY (id_categorie) REFERENCES categories(id_categorie)    
+);
